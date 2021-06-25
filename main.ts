5629609d4e7fa7a4939673c49a29e4888a220e3b
MrMcMahon2021 <84039679+MrMcMahon2021@users.noreply.github.com>
2021-06-25 15:39:46 +0100

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
-
+let mySprite = sprites.create(img`
+    . . . . . . 5 . 5 . . . . . . . 
+    . . . . . f 5 5 5 f f . . . . . 
+    . . . . f 1 5 2 5 1 6 f . . . . 
+    . . . f 1 6 6 6 6 6 1 6 f . . . 
+    . . . f 6 6 f f f f 6 1 f . . . 
+    . . . f 6 f f d d f f 6 f . . . 
+    . . f 6 f d f d d f d f 6 f . . 
+    . . f 6 f d 3 d d 3 d f 6 f . . 
+    . . f 6 6 f d d d d f 6 6 f . . 
+    . f 6 6 f 3 f f f f 3 f 6 6 f . 
+    . . f f d 3 5 3 3 5 3 3 f f . . 
+    . . f d f f 3 5 5 3 f d f . . . 
+    . . . f f 3 3 3 3 3 f d f . . . 
+    . . . f 3 3 5 3 3 5 3 f f . . . 
+    . . . f f f f f f f f f . . . . 
+    . . . . . f f . . . . . . . . . 
+    `, SpriteKind.Player)
+tiles.setTilemap(tilemap`level1`)
+controller.moveSprite(mySprite)
+scene.cameraFollowSprite(mySprite)
